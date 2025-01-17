144a257247fa4d7b14110e38dc5eb51a561b27eb
Charlotte Croce <90940521+charlottecroce@users.noreply.github.com>
2025-01-17 10:39:28 -0500

    Update setup_script.sh

--- net-sec-controls-sec350/setup_script.sh
+++ net-sec-controls-sec350/setup_script.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
 function sethostname(){
-  echo "current hostname: " $(hostname)
-  echo "new hostname (q to exit): "
-  read newHostname
-  echo "executing...'hostnamectl set-hostname " $(newHostname)"'"
-  hostnamectl set-hostname newhostname
-  echo "hostname: " $(hostname)
+	echo "current hostname: " $(hostname)
+	echo "new hostname (q to exit): "
+	read newHostname
+	echo "executing...'hostnamectl set-hostname " $(newHostname)"'"
+	hostnamectl set-hostname newhostname
+	echo "hostname: " $(hostname)
 }
 
+
+user=$(whoami)
+if [[ "$user" != "root" ]]; then
+	echo "please run as root. exiting..."
+	exit 0
 
 while :
 do
 	echo "PLease select an option:"
 	echo "[1] Set Hostname"
 	echo "[2] Set IP"
-  echo "[7] Quit"
+	echo "[7] Quit"
 
 	read userInput
 	echo ""

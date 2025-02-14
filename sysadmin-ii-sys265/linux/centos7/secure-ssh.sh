e40550c7a3613c0c6f82c2fac2e908e830435e62
charlottecroce <charlotte.croce@mymail.champlain.edu>
2025-02-13 22:16:56 -0500

    web-01 created folders

---
+++ sysadmin-ii-sys265/linux/centos7/secure-ssh.sh
@@ -0,0 +1,6 @@
+#secure-ssh.sh
+# creates a new SSH user using $1 parameter
+# adds a public key from the local repo or curled from the remote repo
+# removes roots ability to SSH in
+
+echo "code will go here"

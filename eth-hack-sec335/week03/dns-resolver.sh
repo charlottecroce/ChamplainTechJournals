fbf04baf01a69abcefc865cbe4fe24967843dedb
charlottecroce <charlotte.croce@mymail.champlain.edu>
2026-01-27 10:58:48 -0500

    week 3 dns resolver2

--- eth-hack-sec335/week03/dns-resolver.sh
+++ eth-hack-sec335/week03/dns-resolver.sh
@@ -6,10 +6,10 @@ echo "ip,hostname"
 for i in $(seq 1 254); do
   ip="${network}.${i}"
   # reverse DNS lookup
-  hostname=$(nslookup "$ip" "$dns_server" 2>/dev/null | grep 'name =')
+  hostname=$(nslookup "$ip" "$dns_server" 2>/dev/null | grep 'name =' | awk '{print $NF}')
 
   # only output if a hostname was found
   if [ -n "$hostname" ]; then
-    echo "$ip,$hostname"
+    echo -e "$ip\t$hostname"
   fi
 done

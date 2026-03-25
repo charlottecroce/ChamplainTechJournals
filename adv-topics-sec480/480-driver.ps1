ae88a7e77969fe3d5f9c06e29b8fae5fe4ee5c63
charlottecroce <charlotte.croce@mymail.champlain.edu>
2026-03-25 11:20:20 -0400

    1120

--- adv-topics-sec480/480-driver.ps1
+++ adv-topics-sec480/480-driver.ps1
@@ -1,11 +1,12 @@
 #update module
 Import-Module '480-utils' -Force
 # show banner
-480Banner
+#480Banner
 # get configuration
 $conf = Get-480Config -config_path "480.json"
 # connect to server
-480Connect -server $conf.vcenter_server
+#480Connect -server $conf.vcenter_server
 
 #Write-Host "selecting your VM"
 #Select-VM -folder "BASEVM"
+
